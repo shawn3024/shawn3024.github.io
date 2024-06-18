cc18b6b466352e2bd083d2d581929e80fe4a06b8
shawn3024 <90170705+shawnnordstrom@users.noreply.github.com>
2024-06-17 21:03:31 -0400

    corrected last version

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,15 @@ window.onscroll = function() {
 };
 
 var navbar = document.getElementById("navbar");
+var navbarPlaceholder = document.getElementById("navbar-placeholder")
 var sticky = navbar.offsetTop;
 
 function stickyNavbar() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
+        navbarPlaceholder.style.height = "120px"
     } else {
         navbar.classList.remove("sticky");
+        navbarPlaceholder.style.height = "60px"
     }
 }
